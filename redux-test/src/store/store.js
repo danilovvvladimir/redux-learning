1ec8aa9518c12d4495d7d72fa2ceb54958748f32
Fruity <fruityflunt@gmail.com>
2023-04-22 10:21:26 +0300

    Redux & TS: theory

--- redux-test/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import { combineReducers } from "redux";
-import { cashReducer } from "./reducers/accountReducer";
-
-const rootReducer = combineReducers({
-  cash: cashReducer,
-});
-
-export const store = createStore(rootReducer, {});
